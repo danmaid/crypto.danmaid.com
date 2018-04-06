a3145a176901e1ff33b168a3b22d88614cb08b58
danmaid <yamada@danmaid.com>
2018-04-07 02:18:22 +0900

    exchange 追加

--- modules/src/executions2.js
+++ modules/src/executions2.js
@@ -8,6 +8,121 @@
 ///// bitflyer
 // require pubnub.js
 // <script src="https://cdn.pubnub.com/sdk/javascript/pubnub.4.20.2.js"></script>
+// BTC_JPY
+export const bitflyer_BTC_JPY = function () {
+    let target = new EventTarget()
+    let pubnub = new PubNub({
+        subscribeKey: 'sub-c-52a9ab50-291b-11e5-baaa-0619f8945a4f'
+    });
+    pubnub.addListener({
+        message: (msg) => {
+            msg.message.forEach(elem => {
+                elem.date = new Date(elem.exec_date);
+                elem.volume = elem.size;
+                target.dispatchEvent(new CustomEvent('message', { detail: elem }));
+            })
+        }
+    });
+    pubnub.subscribe({
+        channels: ['lightning_executions_BTC_JPY']
+    });
+    return target
+}()
+
+// FX_BTC_JPY
+export const bitflyer_FX_BTC_JPY = function () {
+    let target = new EventTarget()
+    let pubnub = new PubNub({
+        subscribeKey: 'sub-c-52a9ab50-291b-11e5-baaa-0619f8945a4f'
+    });
+    pubnub.addListener({
+        message: (msg) => {
+            msg.message.forEach(elem => {
+                elem.date = new Date(elem.exec_date);
+                elem.volume = elem.size;
+                target.dispatchEvent(new CustomEvent('message', { detail: elem }));
+            })
+        }
+    });
+    pubnub.subscribe({
+        channels: ['lightning_executions_FX_BTC_JPY']
+    });
+    return target
+}()
+
+///// Zaif
+// BTC_JPY
+export const zaif_BTC_JPY = function () {
+    let target = new EventTarget()
+    let wss = new WebSocket('wss://ws.zaif.jp/stream?currency_pair=btc_jpy')
+    let tid = 0
+    wss.onopen = function () {
+    };
+    wss.onmessage = function (msg) {
+        let data = JSON.parse(msg.data)
+        data = data.trades.filter(trade => tid < trade.tid)
+        data = data.sort((a, b) => a.date - b.date)
+        data.forEach(trade => {
+            trade.volume = trade.amount
+            trade.date = new Date(trade.date * 1000)
+            trade.side = trade.trade_type == 'ask' ? 'SELL' : 'BUY'
+            target.dispatchEvent(new CustomEvent('message', { detail: trade }))
+            tid = trade.tid
+        })
+    };
+    return target
+}()
+
+///// bitbank
+// BTC_JPY
+export const bitbank_BTC_JPY = function () {
+    let target = new EventTarget()
+    let exec = new PubNub({
+        subscribeKey: 'sub-c-e12e9174-dd60-11e6-806b-02ee2ddab7fe'
+    });
+    exec.addListener({
+        message: (msg) => {
+            msg = msg.message.data.transactions.sort((a, b) => a.executed_at - b.executed_at)
+            msg.forEach(elem => {
+                elem.date = new Date(elem.executed_at);
+                elem.volume = parseFloat(elem.amount);
+                elem.price = parseFloat(elem.price);
+                elem.side = elem.side.toUpperCase();
+                target.dispatchEvent(new CustomEvent('message', { detail: elem }));
+            })
+        }
+    });
+    exec.subscribe({
+        channels: ['transactions_btc_jpy']
+    });
+    return target
+}()
+
+///// coincheck
+// BTC_JPY
+export const coincheck_BTC_JPY = function () {
+    let target = new EventTarget()
+    let wss = new WebSocket('wss://ws-api.coincheck.com/')
+    wss.onopen = function () {
+        wss.send(JSON.stringify({
+            "type": "subscribe",
+            "channel": "btc_jpy-trades"
+        }));
+    };
+    wss.onmessage = function (msg) {
+        let data = JSON.parse(msg.data)
+        target.dispatchEvent(new CustomEvent('message', {
+            detail: {
+                price: parseFloat(data[2]),
+                volume: parseFloat(data[3]),
+                side: data[4].toUpperCase(),
+                date: new Date()
+            }
+        }))
+    }
+    return target
+}()
+
 
 ///// bitfinex
 // BTC_USD
@@ -123,16 +238,93 @@ export const okcoin_BTC_USD = function () {
             date.setUTCHours(parseInt(elem[3].split(':')[0]) - 8)
             date.setUTCMinutes(parseInt(elem[3].split(':')[1]))
             date.setUTCSeconds(parseInt(elem[3].split(':')[2]))
+            if ((parseInt(elem[3].split(':')[0]) - 8) < 0) {
+                date.setUTCDate(date.getUTCDate() + 1)
+            }
             target.dispatchEvent(new CustomEvent('message', {
                 detail: {
                     price: parseFloat(elem[1]),
                     volume: parseFloat(elem[2]),
                     date: date,
-                side: elem[4] == 'bid' ? 'BUY' : 'SELL',
-                raw: elem
+                    side: elem[4] == 'bid' ? 'BUY' : 'SELL',
+                    raw: elem
                 }
             }))
-})
+        })
     };
-return target
+    return target
+}()
+
+///// bitstamp
+// BTC_USD
+export const bitstamp_BTC_USD = function () {
+    let target = new EventTarget();
+    let pusher = new Pusher('de504dc5763aeef9ff52');
+    let channel = pusher.subscribe('live_trades');
+    channel.bind('trade', data => {
+        data.volume = data.amount;
+        data.side = data.type == 0 ? 'BUY' : 'SELL';
+        data.date = new Date(data.timestamp * 1000);
+
+        target.dispatchEvent(new CustomEvent('message', { detail: data }))
+    });
+    return target
+}()
+
+
+///// binance
+// BTC_USDT
+export const binance_BTC_USDT = function () {
+    let target = new EventTarget();
+    let wss = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade')
+    wss.onopen = function () {
+    };
+    wss.onmessage = function (msg) {
+        let data = JSON.parse(msg.data)
+
+        data.price = parseFloat(data.p);
+        data.volume = parseFloat(data.q);
+        data.side = data.m ? 'SELL' : 'BUY';
+        data.date = new Date(data.T);
+
+        target.dispatchEvent(new CustomEvent('message', { detail: data }))
+    };
+    return target
+}()
+
+///// OKEx
+// BTC_USDT
+export const okex_BTC_USDT = function () {
+    let target = new EventTarget()
+    let wss = new WebSocket('wss://real.okex.com:10441/websocket')
+    wss.onopen = function () {
+        wss.send(JSON.stringify({
+            "event": "addChannel",
+            "channel": "ok_sub_spot_btc_usdt_deals"
+        }));
+    };
+    wss.onmessage = function (msg) {
+        let data = JSON.parse(msg.data)[0]
+        if (data.channel != "ok_sub_spot_btc_usdt_deals") { return }
+
+        data.data.forEach(elem => {
+            let date = new Date()
+            date.setUTCHours(parseInt(elem[3].split(':')[0]) - 8)
+            date.setUTCMinutes(parseInt(elem[3].split(':')[1]))
+            date.setUTCSeconds(parseInt(elem[3].split(':')[2]))
+            if ((parseInt(elem[3].split(':')[0]) - 8) < 0) {
+                date.setUTCDate(date.getUTCDate() + 1)
+            }
+            target.dispatchEvent(new CustomEvent('message', {
+                detail: {
+                    price: parseFloat(elem[1]),
+                    volume: parseFloat(elem[2]),
+                    date: date,
+                    side: elem[4] == 'bid' ? 'BUY' : 'SELL',
+                    raw: elem
+                }
+            }))
+        })
+    };
+    return target
 }()
